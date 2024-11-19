5bc7215fe28ecd8994e45244e42282dbde91599a
sohagahammed.siyam <sohagahammed.siyam@kone.com>
2024-11-19 16:45:18 +0200

    add url

--- news_api/utils/database.py
+++ news_api/utils/database.py
@@ -1,12 +1,11 @@
-
 import os
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo.errors import PyMongoError
+from bson import ObjectId
 from typing import List, Dict
 import logging
-
 
 load_dotenv()
 username = os.getenv("MONGO_USERNAME")
@@ -18,7 +17,17 @@ def fetch_news(db_name: str, collection_name: str, limit: int) -> List[Dict]:
         with MongoClient(uri, server_api=ServerApi("1")) as client:
             collection = client[db_name][collection_name]
             all_documents = list(
-                collection.find().limit(limit))
+                collection.find({}, {
+                    "_id": 0, 
+                    "url": 1,
+                    "title": 1, 
+                    "text": 1, 
+                    "main_image": 1, 
+                    "domain": 1, 
+                    "category": 1,
+                    }).limit(limit)
+            )
+            
             return all_documents
     except PyMongoError as e:
         logging.error(f"An error occurred while fetching data: {e}")
