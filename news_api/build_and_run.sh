acc0e598c90a0d4f4c92330d141da543ffbe76ac
sohagahammed.siyam <sohagahammed.siyam@kone.com>
2024-11-19 16:26:36 +0200

    keep the docker clean

--- news_api/build_and_run.sh
+++ news_api/build_and_run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Pull the latest changes
 echo "Pulling latest changes..."
 git pull
 
+# Clean up old Docker containers, images, and volumes
+echo "Cleaning up old Docker containers, images, and volumes..."
+docker container prune -f  # Remove stopped containers
+docker image prune -a -f  # Remove unused images
+docker volume prune -f     # Remove unused volumes
+
+# Build the new Docker image
 echo "Building new Docker image..."
 docker build -t news_api .
 
-echo "Stopping and removing the old container..."
-docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
-
+# Run the new Docker container
 echo "Running the new Docker container..."
-docker run -d -p 8000:8000 --name news_api news_api
+docker run -d -p 8000:8000 news_api
